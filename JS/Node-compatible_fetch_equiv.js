a9ed07dd04ab61b560c039b5c5cde5bdc7a1ee45
Patrick Gendotti <patrick.gendotti@gmail.com>
2020-07-17 10:08:50 -0700

    feat: Add a Node-compatile "fetch" equivalent function for situations involving asynchronous API calls using Node.js with no package install permissions (e.g. online REST API evaluations) using built-in Node modules

---
+++ JS/Node-compatible_fetch_equiv.js
@@ -0,0 +1,34 @@
+const https = require('https');
+
+//uses the basic built-in 'https' module for Node.js to re-create
+//the 'fetch' function available in browser-based JS
+
+//Takes in a url, and returns a promise with the JSON for the URL, which is
+//actually a step farther than the original fetch function, but it can easily
+//be modified to accomodate non-JSON responses
+//Rejects the promise on any errors!
+function fetch_equivalent(url) {
+    return new Promise((resolve, reject) =>
+        (https.get(url,(res) => {
+            let body = "";
+
+            res.on("data", (chunk) => {
+                body += chunk;
+            });
+
+            res.on("end", () => {
+                try {
+                    let json = JSON.parse(body);
+                    resolve(json);
+                } catch (error) {
+                    console.error(error.message);
+                    reject(error);
+                };
+            });
+
+        }).on("error", (error) => {
+            console.error(error.message);
+            reject(error)
+        });
+    );
+}
